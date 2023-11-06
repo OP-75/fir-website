c35098b7632c5bb931eda8371e8111355d946e52
Psarthak <panditsarthak1553@gmail.com>
2023-11-06 14:06:38 +0530

    Bar Chart color added

--- src/pages/CrimeBarchart.js
+++ src/pages/CrimeBarchart.js
@@ -37,6 +37,7 @@ export default function CrimeBarchart() {
                   'rgba(75, 192, 192, 0.6)',
                   'rgba(153, 102, 255, 0.6)',
                   'rgba(255, 159, 64, 0.6)',
+                  'rgba(0, 255, 255, 0.6)'
                 ],
                 borderColor: [
                   'rgba(255, 99, 132, 1)',
@@ -45,6 +46,7 @@ export default function CrimeBarchart() {
                   'rgba(75, 192, 192, 1)',
                   'rgba(153, 102, 255, 1)',
                   'rgba(255, 159, 64, 1)',
+                  'rgba(0, 255, 255, 1)'
                 ],
                 borderWidth: 1,
               },
