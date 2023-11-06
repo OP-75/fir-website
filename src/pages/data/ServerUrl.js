1cb3595b9f344a910b1ac5fb5a385fbeddf7aa2d
OP-75 <hparyani75@gmail.com>
2023-11-06 10:07:37 +0530

    added comments

--- src/pages/data/ServerUrl.js
+++ src/pages/data/ServerUrl.js
@@ -1,4 +1,4 @@
 // const server_url = `http://localhost:5000` //for devlopment
-const server_url = `https://fir-website-backend.vercel.app` // for deployment on vercel
+const server_url = `https://fir-website-backend.vercel.app` // for deployment on vercel, besure to uncomment this and comment above url before pushing
 
 export default server_url;
