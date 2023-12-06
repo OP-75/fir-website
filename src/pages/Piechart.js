15838fda7876dcddf34705c10f5025c952a71bcd
OP-75 <hparyani75@gmail.com>
2023-12-06 20:13:21 +0530

    Fixed Piechart.js import which caused the website to crash

--- src/pages/Piechart.js
+++ src/pages/Piechart.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Pie} from "react-chartjs-2";
+import Chart from 'chart.js/auto'; //! DO NOT REMOVE or else everything will break
 
 
 export default function Piechart(props){
